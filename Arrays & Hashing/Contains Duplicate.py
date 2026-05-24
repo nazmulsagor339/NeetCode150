9bc1c0055e65847e475b6d063f7caa4bcd883d1e
Nazmul Haque <nazmulsagor339@gmail.com>
2026-05-24 08:23:26 +0600

    Solution of Contains Duplicate and Valid Anagram

---
+++ Arrays & Hashing/Contains Duplicate.py
@@ -0,0 +1,12 @@
+#Problem Link: https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        temp_set = {}
+        if len(nums)<2:
+            return False
+        for i in range(len(nums)):
+            if nums[i] in temp_set:
+                return True
+            temp_set[nums[i]] = nums[i]
+        return False
